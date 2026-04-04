07c1d5f03a300757f9c6a9781295e918e5deb52d
Marcello Barnaba <vjt@openssl.it>
2026-04-05 00:42:01 +0100

    feat: add HA integration constants and translations

---
+++ custom_components/eisenberg/const.py
@@ -0,0 +1,12 @@
+"""Constants for the Eisenberg integration."""
+
+DOMAIN = "eisenberg"
+
+CONF_DEVICE_ID = "device_id"
+CONF_TRUST_COOKIE = "trust_cookie"
+CONF_MEDIA_DIR = "media_dir"
+CONF_DETECTION_TIMEOUT = "detection_timeout"
+
+DEFAULT_DETECTION_TIMEOUT = 30
+
+EVENT_MEDIA = "eisenberg_media"
